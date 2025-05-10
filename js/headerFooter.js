c580418a9987b08a4a6dc8e4668d96be836284eb
CupcakeFlip <70489875+CupcakeFlip@users.noreply.github.com>
2025-05-10 22:01:20 +0200

    Made it more smooth, changed som name, and made menubar transparent

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -4,12 +4,14 @@ function toggleTopMenu() {
   var menuIcon = document.querySelector(".menuIcon i");
 
   if (menu.classList.contains("open")) {
+    //collapse the menu
     menu.classList.remove("open");
-    topBar.classList.remove("collapsed");
+    topBar.classList.remove("open");
     menuIcon.style.transform = "rotate(0deg)";
   } else {
+    // Open the menu
     menu.classList.add("open");
-    topBar.classList.add("collapsed");
+    topBar.classList.add("open");
     menuIcon.style.transform = "rotate(90deg)";
   }
 }

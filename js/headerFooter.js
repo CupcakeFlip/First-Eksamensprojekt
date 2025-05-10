9a4f81e070c1ddc6dff82a835831cc992a33d390
CupcakeFlip <70489875+CupcakeFlip@users.noreply.github.com>
2025-05-10 19:02:42 +0200

    Added header

---
+++ js/headerFooter.js
@@ -0,0 +1,9 @@
+function toggleTopMenu() {
+  var menu = document.getElementById("topNavMenu");
+  if (menu.style.display === "none") {
+    menu.style.display = "flex";
+    tran
+  } else {
+    menu.style.display = "none";
+  }
+}
